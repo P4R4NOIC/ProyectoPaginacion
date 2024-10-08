ceeeb9ae1264de4135a23236521524c03ed022ce
P4R4NOIC <rvindas-hernandez@hotmail.com>
2024-10-07 22:58:51 -0600

    Update prueba.js

--- prueba.js
+++ prueba.js
@@ -37,6 +37,24 @@ class MMU {
         this.paginas = 0;
         this.tablaPaginasFisicas = [];
         this.tablaPaginasVirtuales = [];
+
+        let pnt = new PTR(1,250);
+        let pnt2 = new PTR(2,50);
+        let pnt3 = new PTR(3,5320);
+        let pnt4 = new PTR(4,345);
+        this.symbolTable.push([1, [pnt, pnt2]]);
+        this.symbolTable.push([2, [pnt3]]);
+        this.symbolTable.push([3, [pnt4]]);
+
+        let page = new PAGE(1,0,0);
+        let page2 = new PAGE(1,1,0);
+        let page3 = new PAGE(2,2,0);
+        let page4 = new PAGE(3,3,0);
+        
+
+        this.memoryMap.push([1, [page, page2]]);
+        this.memoryMap.push([2, [page3]]);
+        this.memoryMap.push([3, [page4]]);
     }
 
     generarPID(){
@@ -105,31 +123,63 @@ class MMU {
         return newPTR;
     }
 
-    use(PTR){
+    use(ptr){
         for (let i = 0; i < this.memoryMap.length; i++) {
-            if(this.memoryMap[i][0] == PTR){
+            if(this.memoryMap[i][0] == ptr){
                 //console.log("PUNTERO "+PTR+" USADO");
             }
             
         }
     }
 
-    delete(PTR){
-        for (let i = 0; i < this.memoryMap.length; i++) {
-            if(this.memoryMap[i][0] == PTR){
-                this.memoryMap.splice(i,1);
-                //console.log("PUNTERO "+PTR+" ELIMINADO");
+    // elimina el ptr de la ts
+    // eliminar toda las paginas del ptr y el ptr del memoMap
+    // en la parte de creacion de operaciones este existira si existe un ptr en la ts
+    delete(ptr){
+        let lista = this.symbolTable;
+        let mensaje = "No se encontro el puntero";
+        lista.forEach(ptrList => {
+            // Filtrar los punteros que no coinciden con el ptr pasado
+            let originalLength = ptrList[1].length;
+            ptrList[1] = ptrList[1].filter(pointer => pointer.pid !== ptr);
+    
+            // Si la longitud original es mayor que la nueva, significa que se eliminó un puntero
+            if (originalLength > ptrList[1].length) {
+                mensaje = "Puntero: " + ptr + " eliminado";
+                let memoryMap = this.memoryMap;
+                for (let i = 0; i < memoryMap.length; i++) {
+                    if (memoryMap[i][0] == ptr) {
+                        memoryMap.splice(i, 1); // Elimina el elemento en la posición i
+                        console.log(`Elemento con ptr ${ptr} eliminado del memoryMap.`);
+                        break; // Sale del bucle una vez que el elemento es eliminado
+                    }
+                }
             }
-        }
+        });
+
+        
+        console.log(mensaje);
     }
 
+    // elimina el proceso de la tabla de simbolos con sus respectivos ptr que a su ves deberan borrarse del memoMap con sus respectivas paginas
+    // en la parte de creacion de operaciones este saldra solo una vez por pid y aparecer solo al final del proceso es decir no pueden haber operaciones con ese proceso despues de su kill
+    // deletedPIDs.push(pid);
     kill(pid){
-        for (let i = 0; i < this.memoryMap.length; i++) {
-            // if(this.tabla[i][1].pid == pid){
-            //     this.tabla.splice(i,1);
-            //     console.log("PUNTERO "+pid+" MATADO");
-            // }
-        }
+        let listaSimbolos = this.symbolTable;
+        
+        
+        listaSimbolos.forEach(element => {
+            if(element[0]==pid){
+                element[1].forEach(ptr => {
+                    this.delete(ptr.pid);
+                });
+                
+            }
+        });
+
+        this.symbolTable = listaSimbolos.filter(element => element[0] !== pid);
+
+        // deletedPIDS.push(pid);
     }
 }
 //funcion que asigna la direccion inicial de memoria de la pagina
@@ -141,15 +191,15 @@ function assignSegment(){
 
 // Ejemplo de uso:
 let newMMU = new MMU();
-newMMU.new(1,250);
-newMMU.new(1,50);
-newMMU.new(2,5320);
-newMMU.new(3,345);
-newMMU.use(1);
-newMMU.use(3);
-newMMU.use(2);
-newMMU.use(1);
-//console.log(newMMU);
+// newMMU.new(1,250);
+// newMMU.new(1,50);
+// newMMU.new(2,5320);
+// newMMU.new(3,345);
+// newMMU.use(1);
+// newMMU.use(3);
+// newMMU.use(2);
+// newMMU.use(1);
+console.log(newMMU);
 newMMU.delete(1);
 newMMU.kill(1);
 newMMU.kill(2);
